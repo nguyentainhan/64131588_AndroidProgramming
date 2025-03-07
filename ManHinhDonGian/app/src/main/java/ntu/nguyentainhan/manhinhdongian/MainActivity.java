cd1db0024777abbf33d8b8981dcdfffe22e9226e
nguyentainhan <ntnhan852017@gmail.com>
2025-03-07 16:22:54 +0700

    Bài thực hành về chuyển màn hình đơn giản

--- ManHinhDonGian/app/src/main/java/ntu/nguyentainhan/manhinhdongian/MainActivity.java
+++ ManHinhDonGian/app/src/main/java/ntu/nguyentainhan/manhinhdongian/MainActivity.java
@@ -1,6 +1,9 @@
 package ntu.nguyentainhan.manhinhdongian;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,12 +12,42 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button nutMH2;
+    Button nutMH3;
+    void TimDieuKhien(){
+        nutMH2 = (Button) findViewById(R.id.btnMH2);
+        nutMH3 = (Button) findViewById(R.id.btnMH3);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        //Tim điều khiển
+        TimDieuKhien();
+        //Gắn bộ lắng nghe sự kiện
+        nutMH2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Xử lý chuyển màn hình
+                //B1. Tạo 1 Intent
+                Intent intentMH2 = new Intent(MainActivity.this, MH2Activity.class);
+                //B2. Gửi
+                startActivity(intentMH2);
+            }
+        });
+
+        nutMH3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Xử lý chuyển màn hình
+                //B1. Tạo 1 Intent
+                Intent intentMH3 = new Intent(MainActivity.this, MH3Activity.class);
+                //B2. Gửi
+                startActivity(intentMH3);
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
