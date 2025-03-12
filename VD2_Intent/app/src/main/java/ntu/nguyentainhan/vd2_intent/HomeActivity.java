7206a17664cc3386a2992dc78a48964452d348e3
nguyentainhan <ntnhan852017@gmail.com>
2025-03-12 18:34:26 +0700

    Ví dụ 2
    đã hoàn thành

---
+++ VD2_Intent/app/src/main/java/ntu/nguyentainhan/vd2_intent/HomeActivity.java
@@ -0,0 +1,31 @@
+package ntu.nguyentainhan.vd2_intent;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_home);
+        //lấy intent về
+        Intent intentTvLogin = getIntent();
+        String tenDN_Nhanduoc = intentTvLogin.getStringExtra("ten_dang_nhap");
+        TextView tvtenDN = (TextView) findViewById(R.id.tvUserName);
+        tvtenDN.setText(tenDN_Nhanduoc);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
---
+++ VD2_Intent/app/src/main/java/ntu/nguyentainhan/vd2_intent/MainActivity.java
@@ -0,0 +1,37 @@
+package ntu.nguyentainhan.vd2_intent;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        //tìm điều kiện
+        Button btnDangNhap = (Button) findViewById(R.id.btnLogin);
+        // Sự kiện
+        btnDangNhap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent iLogin = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(iLogin);
+            }
+        });
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
