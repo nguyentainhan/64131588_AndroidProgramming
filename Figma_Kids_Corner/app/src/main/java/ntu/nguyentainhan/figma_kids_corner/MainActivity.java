e1492f9a46a83b2aabb4337375b61fb1ceadb79f
nguyentainhan <ntnhan852017@gmail.com>
2025-03-24 16:44:11 +0700

    dang lam giua chung

--- Figma_Kids_Corner/app/src/main/java/ntu/nguyentainhan/figma_kids_corner/MainActivity.java
+++ Figma_Kids_Corner/app/src/main/java/ntu/nguyentainhan/figma_kids_corner/MainActivity.java
@@ -1,6 +1,9 @@
 package ntu.nguyentainhan.figma_kids_corner;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +12,21 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        // Ánh xạ TextView
+        TextView tvTieude = findViewById(R.id.tvTieude);
+
+        // Xử lý sự kiện Click để chuyển màn hình
+        tvTieude.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, HomePage.class);
+                startActivity(intent);
+            }
         });
     }
 }
